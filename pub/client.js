e750736e91e23e65fcb3877a18df15105b6b1bad
System <none>
2017-05-05 16:38:25 +0000

    Progress on importer

--- pub/client.js
+++ pub/client.js
@@ -1,7 +1,11 @@
+'use strict';
 // --------------------------------- 80chars ---------------------------------->
-var TWURL = 'http://twitter.com/beemuvi';
-var BICON = // tiny twitter birdie icon
+const TURL = 'https://twitter.com/';
+const BURL = TURL + 'beemuvi';
+const BICON = // tiny twitter birdie icon
      'https://cdn.glitch.com/048f1230-830a-4702-9106-1d28c7e8a2c9%2Fbirdie.png';
+
+var n = 0; // global variable counting the UVIs as we generate them
 
 // Wrap bare URLs in string s with "a href" tags; also Twitter @-mentions
 function linkify(s) {
@@ -25,27 +29,33 @@ function genhov(d, t, c) { // if just d's given let it be ambiguous
   return s
 }
 
-// Takes UVI object and generates the html <li> element. Fields:
+// Takes UVI object and generates the html <li> element
 function genli(uvi) {
-  var num     = uvi.n; // n: the number of the UVI (can omit if prev+1)
-  var feat    = uvi.f; // f: whether to highlight the UVI (boolean)
-  var text    = uvi.x; // x: the full text of the UVI
-  var twurl   = uvi.u; // u: URL of the tweet
-  var date    = uvi.d; // d: date the UVI was deployed
-  var twate   = uvi.t; // t: date the UVI was tweeted
-  var comment = uvi.c; // c: notes to selves / hovertext on link to tweet
+  n += 1; // increment the global variable for the number of the UVI
+
+  var num     = uvi.n || n;   // n: the number of the UVI (can omit if prev+1)
+  var feat    = uvi.f;        // f: whether to highlight the UVI (boolean)
+  var text    = uvi.x;        // x: the full text of the UVI
+  var twurl   = uvi.u;        // u: URL of the tweet
+  var date    = uvi.d;        // d: date the UVI was deployed
+  var twate   = uvi.t;        // t: date the UVI was tweeted
+  var comment = uvi.c;        // c: notes to selves / hovertext on link to tweet
+
+  if (num !== n) {
+    console.log(`UVI NUMBERING ERROR: ${n-1} -> ${num}`);
+    n = num
+  }
   
   if (!text) { text = "ERROR: "+JSON.stringify(uvi) }
   
-  return '<a name="'+num+'"></a>'
-    + '<li value="'+num+'">'
+  return '<li value="'+n+'">' + '<a name="'+n+'"></a>'
     + linkify(feat ? '<strong>'+text+'</strong>' : text)
-    + ' <a href="'+(twurl ? twurl : TWURL)+'" title="'
+    + ' <a href="'+(twurl ? twurl : BURL)+'" title="'
     + genhov(date, twate, comment)
     + '">'
     + '<img src="'+BICON+'"/>'
     + '</a>'
-    '</li>\n'
+    + '</li>\n'
 }
 
 // Generate html for the ordered list of UVIs
